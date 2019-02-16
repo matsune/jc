81551f388f82c5abe0750af913b9597fad15a1ac
Yuma Matsune <yuma.matsune@gmail.com>
2019-02-16 12:41:26 +0900

    read ~/.jc.conf

--- cmd/jc/main.go
+++ cmd/jc/main.go
@@ -4,34 +4,119 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/user"
+	"path/filepath"
+	"strconv"
+	"strings"
 
+	"github.com/BurntSushi/toml"
 	"github.com/fatih/color"
 	"github.com/matsune/jc"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+type conf struct {
+	Key    string
+	Number string
+	String string
+	Bool   string
+	Null   string
+}
+
 func main() {
-	if !terminal.IsTerminal(0) {
-		b, err := ioutil.ReadAll(os.Stdin)
+	if terminal.IsTerminal(0) {
+		return
+	}
+
+	// read from pipe
+	b, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	c := conf{
+		Key:    "",
+		Number: "34",
+		String: "33",
+		Bool:   "31",
+		Null:   "36",
+	}
+
+	confPath := filepath.Join(usr.HomeDir, ".jc.conf")
+	if _, err = os.Stat(confPath); !os.IsNotExist(err) {
+		_, err = toml.DecodeFile(confPath, &c)
 		if err != nil {
 			fmt.Fprint(os.Stderr, err)
 			os.Exit(1)
 		}
-
-		keyColor := color.New(color.FgCyan)
-		numColor := color.New(color.BgBlue, color.Bold)
-		strColor := color.New(color.FgWhite, color.BgBlack)
-		boolColor := color.New(color.FgHiBlue)
-		nullColor := color.New(color.FgHiBlue, color.BgHiRed)
-		j := jc.New(
-			jc.KeyColor(keyColor),
-			jc.NumberColor(numColor),
-			jc.StringColor(strColor),
-			jc.BoolColor(boolColor),
-			jc.NullColor(nullColor),
-		)
-		if err := j.Colorize(string(b)); err != nil {
-			panic(err)
-		}
 	}
+
+	keys, err := splitAttributes(c.Key)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	keyColor := color.New(keys...)
+
+	nums, err := splitAttributes(c.Number)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	numColor := color.New(nums...)
+
+	strings, err := splitAttributes(c.String)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	strColor := color.New(strings...)
+
+	bools, err := splitAttributes(c.Bool)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	boolColor := color.New(bools...)
+
+	nulls, err := splitAttributes(c.Null)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	nullColor := color.New(nulls...)
+
+	j := jc.New(
+		jc.KeyColor(keyColor),
+		jc.NumberColor(numColor),
+		jc.StringColor(strColor),
+		jc.BoolColor(boolColor),
+		jc.NullColor(nullColor),
+	)
+	if err := j.Colorize(string(b)); err != nil {
+		panic(err)
+	}
+}
+
+func splitAttributes(str string) ([]color.Attribute, error) {
+	if len(str) == 0 {
+		return []color.Attribute{}, nil
+	}
+	numStrs := strings.Split(str, ",")
+	attrs := make([]color.Attribute, len(numStrs))
+	for i, numStr := range numStrs {
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			return nil, err
+		}
+		attrs[i] = color.Attribute(num)
+	}
+	return attrs, nil
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
 module github.com/matsune/jc
 
 require (
+	github.com/BurntSushi/toml v0.3.1
 	github.com/fatih/color v1.7.0
 	github.com/mattn/go-colorable v0.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
-	golang.org/x/arch v0.0.0-20181203225421-5a4828bb7045 // indirect
 	golang.org/x/crypto v0.0.0-20190211182817-74369b46fc67
 	golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a // indirect
 )
